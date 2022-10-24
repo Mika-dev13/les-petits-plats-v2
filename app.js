d16e129e9b3b8a9a779fb1893c36462c2204f391
Chorro Michael <michaelchorro.dev@gmail.com>
2022-10-24 21:34:45 +0200

    implementation of search bar + btns

--- app.js
+++ app.js
@@ -1,49 +1,17 @@
 import { RECIPES } from './data/recipes.js';
+import displayRecipeItem from './components/recipeItem.js';
 
 const itemsContainer = document.querySelector('.items-container');
+const filterBtns = document.querySelectorAll('.filter-btn');
 
 window.addEventListener('DOMContentLoaded', () => {
   displayRecipeItem(RECIPES);
   // displayListIngredient();
 });
 
-function displayRecipeItem(recipeItems) {
-  let displayRecipe = recipeItems.map((item) => {
-    let ingredientsList = [];
-
-    for (let ingredient of item.ingredients) {
-      const ingredientCleaned = ingredient.unit
-        ? `<li><strong>${ingredient.ingredient}:</strong> ${ingredient.quantity} ${ingredient.unit}</li>`
-        : ingredient.quantity
-        ? `<li><strong>${ingredient.ingredient}:</strong> ${ingredient.quantity}</li>`
-        : `<li><strong>${ingredient.ingredient}</strong>`;
-      ingredientsList.push(`<li>${ingredientCleaned}</li>`);
-    }
-
-    return `<article class="item">
-    <div class="item-img">
-      <img src="./assets/dummy_600x400_ffffff_cccccc.png" alt="" />
-    </div>
-    <div class="item-content">
-      <div class="item-content-header">
-        <h2>${item.name}</h2>
-        <div class="time">
-          <img src="./assets/clock.svg" alt="" />
-          <span>${item.time} min</span>
-        </div>
-      </div>
-      <div class="item-content-desc">
-        <ul class="ingredients-list">
-            ${ingredientsList.join('')}
-        </ul>
-        <div class="text-description">
-          <p>
-            ${item.description}
-          </p>
-        </div>
-      </div>
-    </div>
-  </article>`;
+filterBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const category = e.currentTarget.dataset.id;
+    console.log(category);
   });
-  itemsContainer.innerHTML = displayRecipe.join('');
-}
+});
---
+++ components/recipeItem.js
@@ -0,0 +1,44 @@
+import { RECIPES } from '../data/recipes.js';
+
+const itemsContainer = document.querySelector('.items-container');
+
+export default function displayRecipeItem(recipeItems) {
+  let displayRecipe = recipeItems.map((item) => {
+    let ingredientsList = [];
+
+    for (let ingredient of item.ingredients) {
+      const ingredientCleaned = ingredient.unit
+        ? `<li><strong>${ingredient.ingredient}:</strong> ${ingredient.quantity} ${ingredient.unit}</li>`
+        : ingredient.quantity
+        ? `<li><strong>${ingredient.ingredient}:</strong> ${ingredient.quantity}</li>`
+        : `<li><strong>${ingredient.ingredient}</strong>`;
+      ingredientsList.push(`<li>${ingredientCleaned}</li>`);
+    }
+
+    return `<article class="item">
+      <div class="item-img">
+        <img src="./assets/dummy_600x400_ffffff_cccccc.png" alt="" />
+      </div>
+      <div class="item-content">
+        <div class="item-content-header">
+          <h2>${item.name}</h2>
+          <div class="time">
+            <img src="./assets/clock.svg" alt="" />
+            <span>${item.time} min</span>
+          </div>
+        </div>
+        <div class="item-content-desc">
+          <ul class="ingredients-list">
+              ${ingredientsList.join('')}
+          </ul>
+          <div class="text-description">
+            <p>
+              ${item.description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>`;
+  });
+  itemsContainer.innerHTML = displayRecipe.join('');
+}
